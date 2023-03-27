681de645c4c29915aa07ff7c38e92da0dd6a9945
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-03-27 17:10:30 +0300

    Create TodoItem.js

---
+++ src/components/TodoItem.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { FaTrash } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import styles from './TodoItem.module.css';
+// eslint-disable-next-line
+const TodoItem = (props) => {
+  const [isEditing, setEditing] = useState(false);
+
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
+
+  const { todo } = props;
+  const { completed, id, title } = todo;
+  const { handleChange, delTodo, setUpdate } = props;
+  const { item, textInput } = styles;
+
+  const handleEditing = () => {
+    setEditing(true);
+  };
+
+  const viewMode = {};
+  const editMode = {};
+
+  if (isEditing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+
+  const handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      setEditing(false);
+    }
+  };
+
+  return (
+    <li className={item}>
+      <div onDoubleClick={handleEditing} style={viewMode}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => handleChange(id)}
+        />
+        <button type="button" onClick={() => delTodo(id)}>
+          <FaTrash style={{ color: 'orangered', fontSize: '16px' }} />
+        </button>
+        <span style={completed ? completedStyle : null}>
+          {title}
+        </span>
+      </div>
+      <input
+        type="text"
+        style={editMode}
+        className={textInput}
+        value={title}
+        onChange={(e) => {
+          setUpdate(e.target.value, id);
+        }}
+        onKeyDown={handleUpdatedDone}
+      />
+    </li>
+  );
+};
+
+TodoItem.propTypes = {
+  todo: PropTypes.shape({
+    completed: PropTypes.bool.isRequired,
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+  handleChange: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+  setUpdate: PropTypes.func.isRequired,
+};
+
+export default TodoItem;

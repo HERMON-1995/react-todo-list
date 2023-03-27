cd18b08ba8b5ca4f4054287c69863e3a032836b6
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-03-27 17:10:13 +0300

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// eslint-disable-next-line
+const Header = () => {
+  const headerStyle = {
+    padding: '20px 0',
+    lineHeight: '1.5em',
+  };
+
+  return (
+    <header style={headerStyle}>
+      <h1 style={{
+        fontSize: '6rem',
+        fontWeight: '600',
+        marginBottom: '2rem',
+        lineHeight: '1em',
+        color: '#ccd',
+        textTransform: 'lowercase',
+        textAlign: 'center',
+      }}
+      >
+        Todos
+      </h1>
+    </header>
+  );
+};
+
+export default Header;

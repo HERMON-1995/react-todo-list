3262e254c47ab15323f3a768b27d1c9e57ccc8d9
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-03-27 17:10:27 +0300

    Create TodoContainer.js

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Route, Routes } from 'react-router-dom';
+import TodosList from './TodosList';
+import Header from './Header';
+import InputTodo from './InputTodo';
+import Navbar from './Navbar';
+import About from '../pages/About';
+import NotMatch from '../pages/NotMatch';
+// eslint-disable-next-line
+const TodoContainer = () => {
+  function getInitialTodos() {
+    const temp = localStorage.getItem('todos');
+    const savedTodos = JSON.parse(temp);
+    return savedTodos || [];
+  }
+
+  const [todos, setTodos] = useState(getInitialTodos());
+
+  useEffect(() => {
+    const temp = localStorage.getItem('todos');
+    const loadedTodos = JSON.parse(temp);
+    if (loadedTodos) {
+      setTodos(loadedTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    const temp = JSON.stringify(todos);
+    localStorage.setItem('todos', temp);
+  }, [todos]);
+
+  const handleChange = (id) => {
+    setTodos(todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    }));
+  };
+
+  const delTodo = (id) => {
+    setTodos(
+      [
+        ...todos.filter((todo) => todo.id !== id),
+      ],
+    );
+  };
+
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+    setTodos(
+      [...todos, newTodo],
+    );
+  };
+
+  const setUpdate = (updatedTitle, id) => {
+    setTodos((prevState) => prevState.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          title: updatedTitle,
+        };
+      }
+      return todo;
+    }));
+  };
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={(
+            <div className="container">
+              <div className="inner">
+                <Header />
+                <InputTodo addTodoProps={addTodoItem} />
+                <TodosList
+                  todos={todos}
+                  handleChange={handleChange}
+                  delTodo={delTodo}
+                  setUpdate={setUpdate}
+                />
+              </div>
+            </div>
+          )}
+        />
+        <Route path="/about/*" element={<About />} />
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
+    </>
+  );
+};
+
+export default TodoContainer;
